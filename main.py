3779cc34a4b96b3f441d495da4516f846aa15d75
JackJackStudios <jackjackfilms1@gmail.com>
2024-12-11 17:27:44 +0000

    Added road tiles

--- main.py
+++ main.py
@@ -11,13 +11,14 @@ class Title(Room):
 class Main(Room):
     def __init__(self, game, primary=False):
         super().__init__(game, primary)
+        self.tilemap.load("Road", 320)
         WalkerAlgorithm((1, 1), pygame.Rect(1, 1, 20, 20), self.tilemap).compute(20)
 
         Car(self, pos=(0, 0), primary=True)
 
 class CarGame(Game):
     def __init__(self) -> None:
-        super().__init__("CS50 Final", 60, (1440, 810), 2)
+        super().__init__("CS50 Final", 60, (1440, 810), 1)
         
         Main(self, primary=True)
         Title(self)
